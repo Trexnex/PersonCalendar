d15ea35f969f28e05b69de5502fdeea4d664e9d5
Trexnex <50576257+Trexnex@users.noreply.github.com>
2022-03-06 01:54:39 +0100

    getMonthStartDay func added

--- Support functions.py
+++ Support functions.py
@@ -5,3 +5,10 @@ def checkForLeap(year): #Returns if a years is a leap year or not.
         return True
     else:
         return False
+
+daysInMonths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+print(datetime.date.fromisocalendar(2021, 52, 7))
+
+def getMonthStartDay(year, month):
+    return datetime.date(year, month, 1).weekday()
