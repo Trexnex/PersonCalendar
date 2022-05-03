2a8359e075e16bac28058cc0d91769aab3df4d06
Trexnex <50576257+Trexnex@users.noreply.github.com>
2022-05-03 03:27:10 +0200

    Calendar UI done and early database functions

--- main.py
+++ main.py
@@ -1,26 +1,44 @@
 import tkinter as tk
 from supportFunctions import *
+from tkcalendar import Calendar
 
 HEIGHT = 350
-WIDTH = 600
+WIDTH = 700
+global month
 
 
+
+month, year = getMonthAndYear()
+print(getMonthStartDay(year, month ))
 frameList = []
 core = tk.Tk()
 core.title("Calendar")
 core.geometry(str(WIDTH)+"x"+str(HEIGHT))
 core.resizable(False, False)
-
-emptyImage = tk.PhotoImage(height=0, width=0)
-
-frameList = generateCalendar(core)
-print(frameList)
-for x in range(6):
-    for i in range(7):
-        tk.Button(frameList[x][i], text="Test", command=print).pack(expand = True, fill = 'both', side=tk.LEFT)
-        frameList[x][i].place(x= x*100, y = i*50)
+picturelist = createPicturelist()
+db, cs = createDatabase()
+#dbAdd(db, cs, "nam", "nicknam", "birthda", "addres", "socialmeadi", "emai", "phon", "birthdayWishe", "wor", "knowsFro", "note")
 
 
+for s in (dbSearch(db, cs, "")):
+    print(s)
+
+
+
+updateCalendar(core, frameList, picturelist, month, year)
+
+startVarMonth = tk.StringVar()
+startVarMonth.set(monthNames[month-1])
+spinboxMonth = tk.Spinbox(core, values = monthNames, width=30, state = "readonly", textvariable = startVarMonth)
+spinboxMonth.place(x=100, y=310)
+startVarYear = tk.StringVar()
+startVarYear.set(year)
+spinboxYear = tk.Spinbox(core, from_ = 1800, to = 10000, width=30, state = "readonly", textvariable = startVarYear)
+spinboxYear.place(x=300, y=310)
+updateCalButton = tk.Button(core, text="Update", command= lambda: updateCalendar(core, frameList, picturelist
+                                                                                , int(monthNames.index(spinboxMonth.get())+1),
+                                                                                int(spinboxYear.get())), height=2, width=10)
+updateCalButton.place(x=500, y=310)
 
 
 core.mainloop()
--- supportFunctions.py
+++ supportFunctions.py
@@ -1,6 +1,15 @@
 import datetime
+import math
 import tkinter as tk
+from PIL import Image, ImageDraw, ImageFont, ImageTk
+import sqlite3
 
+counter = -1
+
+def getMonthAndYear():
+    month = datetime.datetime.today().month
+    year = datetime.datetime.today().year
+    return month, year
 
 def checkForLeap(year): #Returns if a years is a leap year or not.
     if (year%400 == 0 or year%4 == 0 and year % 100 != 0):
@@ -9,8 +18,8 @@ def checkForLeap(year): #Returns if a years is a leap year or not.
         return False
 
 daysInMonths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
-print(datetime.date.fromisocalendar(2021, 52, 7))
 
 def getMonthStartDay(year, month):
     return datetime.date(year, month, 1).weekday()
@@ -18,11 +27,85 @@ def getMonthStartDay(year, month):
 def generateCalendar(core):
     frameList = []
     for i in range(6):
-        coloumn = []
+        rows = []
         for x in range(7):
-            coloumn.append(tk.Frame(core, height = 50, width = 100))
-        frameList.append(coloumn)
+            temp = (tk.Frame(core, height = 50, width = 100))
+            rows.append(temp)
+        frameList.append(rows)
     return frameList
 
-def testFunc():
-    print("Test output")
+def frameClick(event, counter, daysInto):
+    print(counter)
+    event.widget['bg'] = "red"
+    temp = str(event.widget)
+    print(temp)
+    temp = temp.replace(".!frame", "")
+    temp = temp.replace(".!label", "")
+    temp = int(temp) - (counter * 42)
+    if temp == "":
+        print([0, 0])
+    else:
+        temp = int(temp)
+        print(temp-daysInto)
+        xCoord = math.floor((temp-1)/7)
+        yCoord = (temp-1)%7
+        print([xCoord, yCoord])
+
+def makeImage(nummer):
+    font = ImageFont.truetype("arial.ttf", 50)
+    tempImg = Image.new(mode="RGB", size=(100, 50), color=(255, 255, 255))
+    editImg = ImageDraw.Draw(tempImg)
+    editImg.text((0, 0), str(nummer), ("black"), font = font)
+    return tempImg
+
+def createPicturelist():
+    pictureList = []
+    for i in range(45):
+        pictureList.append(ImageTk.PhotoImage(makeImage(i + 1)))
+    return pictureList
+
+def updateCalendar(core, frameList, pictureList, month, year):
+    global counter
+    counter += 1
+    for i in frameList: #Clear the frame
+        for x in i:
+            x.destroy()
+    print(core.winfo_children())
+    for i in core.winfo_children(): #Clear the calendar
+        if str(i).__contains__("frame"):
+            i.destroy()
+    frameList = generateCalendar(core) #Generate a new calendar
+    y = 0
+    y -= getMonthStartDay(year, month) - 1 #Get the start day of the month
+    for i in range(6): #Place elements
+        for x in range(7):
+            if y > 0:
+                if y < daysInMonths[month - 1] + 1:
+                    frameList[i][x].place(y=i * 50, x=x * 100)
+                    tempLabel = tk.Label(frameList[i][x], image=pictureList[y - 1])
+                    tempLabel.bind("<Button-1>", lambda event, o = counter, daysInto = getMonthStartDay(year, month): frameClick(event, o, daysInto)) #Bind a command to left click when clicking on a day
+                    tempLabel.pack()
+            y += 1
+def createDatabase(): #Creates a database
+    db = sqlite3.connect("database.db")
+    cs = db.cursor()
+    cs.execute("CREATE TABLE IF NOT EXISTS people (name TEXT, nickname TEXT, birthday TEXT, adress TEXT, socialmedia TEXT, email TEXT, phone TEXT, birthdayWishes TEXT, work TEXT, knowsFrom TEXT, notes TEXT)")
+    db.commit()
+    return db, cs
+
+
+def dbAdd(db, cs, name, nickname, birthday, adress, socialmedia, email, phone, birthdayWishes, work, knowsFrom, notes): #Adds a person to the database
+    cs.execute("INSERT INTO people VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (name, nickname, birthday, adress, socialmedia, email, phone, birthdayWishes, work, knowsFrom, notes))
+    db.commit()
+
+def dbSearch(db, cs, name): #Searches for a person in the database
+    cs.execute("SELECT * FROM people WHERE name LIKE ?", ("%"+name+"%",))
+    return cs.fetchall()
+
+def dbDelete(db, cs, name): #Deletes a person from the database
+    cs.execute("DELETE FROM people WHERE name = ?", (name))
+    db.commit()
+
+def dbUpdate(db, cs, name, nickname, birthday, adress, socialmedia, email, phone, birthdayWishes, work, knowsFrom, notes): #Updates a person in the database
+    cs.execute("UPDATE people SET nickname = ?, birthday = ?, address = ?, socialmedia = ?, email = ?, phone = ?, birthdayWishes = ?, work = ?, knowsFrom = ?, notes = ? WHERE name = ?", (nickname, birthday, adress, socialmedia, email, phone, birthdayWishes, work, knowsFrom, notes, name))
+    db.commit()
